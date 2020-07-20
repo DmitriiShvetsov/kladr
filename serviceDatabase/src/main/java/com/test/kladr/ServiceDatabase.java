76d7b13016bcb0d744acb2bc96b2495e2621fed5
DmitriiShvetsov <dmitrii.shvetsov@gmail.com>
2020-07-20 13:55:55 +0400

    new gateway

--- serviceDatabase/src/main/java/com/test/kladr/ServiceDatabase.java
+++ serviceDatabase/src/main/java/com/test/kladr/ServiceDatabase.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KladrApplication {
+public class ServiceDatabase {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KladrApplication.class, args);
+		SpringApplication.run(ServiceDatabase.class, args);
 	}
 
 }
